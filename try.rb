387a6545a3fb813f7d46b40890293ab94ef1ffc9
GladkovKostas <kostasgladkov@gmail.com>
2013-09-26 14:35:11 +0300

    now_its_array

--- try.rb
+++ try.rb
@@ -1,34 +1,27 @@
-counter = 0
-array = (1..15)
+diap = (1..15)
 a = []
 numbers = []
 
-array.each do |i|
+diap.to_a.each do |i|
 	if i % 3 == 0
-		counter += 1
 		a.push(i)
 	end
 end
 
-
-a.each do |i|
+diap.to_a.each do |i|
 	if i % 3 == 0 and i % 5 == 0
-		counter -= 1
 		else
 		numbers.push(i)
 	end
 end
 
-array.each do |i|
+diap.to_a.each do |i|
 	if i % 5 == 0
-		counter += 1
 		numbers.push(i)
 	end
 end
 
-
-
 puts "quantity"
-puts counter
+puts numbers.length
 puts "numbers"
 puts numbers.sort
