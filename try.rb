21e72aad2c31b336ed1157c1ef52d598424e01ab
GladkovKostas <kostasgladkov@gmail.com>
2013-09-26 13:47:53 +0300

    without_counter

--- try.rb
+++ try.rb
@@ -1,19 +1,15 @@
-counter = 0
 array = (1..15)
 a = []
 numbers = []
 
 array.each do |i|
 	if i % 3 == 0
-		counter += 1
 		a.push(i)
 	end
 end
 
-
 a.each do |i|
 	if i % 3 == 0 and i % 5 == 0
-		counter -= 1
 		else
 		numbers.push(i)
 	end
@@ -21,14 +17,11 @@ end
 
 array.each do |i|
 	if i % 5 == 0
-		counter += 1
 		numbers.push(i)
 	end
 end
 
-
-
 puts "quantity"
-puts counter
+puts numbers.length
 puts "numbers"
 puts numbers.sort
